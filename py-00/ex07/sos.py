174cdfe08ce3119b4fceadd2302ec82c8967e1f1
BenjDW <dewitskibenji@live.fr>
2025-10-01 07:20:31 -0700

    doc and norm apply

--- py-00/ex07/sos.py
+++ py-00/ex07/sos.py
@@ -1,34 +1,49 @@
 import sys
 
+
 def main():
-	dictionnary = { "A": ".-", "B": "-...",  "C": "-.-.", "D": "-..", 
-					"E": ".",  "F": "..-.",  "G": "--.", "H": "....",
-					"I": "..", "J": ".---", "K": "-.-", "L": ".-..",
-					"M": "--", "N": "-.",   "O": "---", "P": ".--.",
-					"Q": "--.-", "R": ".-.", "S": "...", "T": "-",
-					"U": "..-", "V": "...-", "W": ".--", "X": "-..-",
-					"Y": "-.--", "Z": "--..",
-					"0": "-----", "1": ".----", "2": "..---", "3": "...--",
-					"4": "....-", "5": ".....", "6": "-....", "7": "--...",
-					"8": "---..", "9": "----.",
-					" ": "/"}
-	
-	if len(sys.argv) != 2:
-		raise AssertionError("the arguments are bad")
-	string = sys.argv[1].upper()
+    """
+    Module <SOS>
 
-	if any(c not in dictionnary for c in string):
-		raise AssertionError("the arguments are bad")
+    only display the morse code of the given string
 
-	# for c in string:
-	# 	if c not in dictionnary:
-	# 		raise AssertionError("the arguments are bad")
+    Args:
+        none, sys.argv is used for taking the string to convert
 
-	morse = " ".join([dictionnary[c] for c in string])
-	print(morse)
+    Returns:
+        none
+
+    Raises:
+        AssertionError: if the number of args is not good
+        or the character is not contains in the dict
+
+    """
+    dictionnary = {
+        "A": ".-", "B": "-...",  "C": "-.-.", "D": "-..",
+        "E": ".",  "F": "..-.",  "G": "--.", "H": "....",
+        "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
+        "M": "--", "N": "-.",   "O": "---", "P": ".--.",
+        "Q": "--.-", "R": ".-.", "S": "...", "T": "-",
+        "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
+        "Y": "-.--", "Z": "--..",
+        "0": "-----", "1": ".----", "2": "..---", "3": "...--",
+        "4": "....-", "5": ".....", "6": "-....", "7": "--...",
+        "8": "---..", "9": "----.",
+        " ": "/"}
+
+    if len(sys.argv) != 2:
+        raise AssertionError("the arguments are bad")
+    string = sys.argv[1].upper()
+
+    if any(c not in dictionnary for c in string):
+        raise AssertionError("the arguments are bad")
+
+    morse = " ".join([dictionnary[c] for c in string])
+    print(morse)
+
 
 if __name__ == "__main__":
-	try:
-		main()
-	except AssertionError as e:
-		print(f"AssertionError: {e}")
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
