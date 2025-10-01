d14aa416d742558ce0fd7cbc9cb778ea859814e1
BenjDW <dewitskibenji@live.fr>
2025-10-01 06:57:39 -0700

    doc and norm apply

--- py-00/ex06/filterstring.py
+++ py-00/ex06/filterstring.py
@@ -1,26 +1,47 @@
 import sys
 from ft_filter import ft_filter
 
-def main():
-	if len(sys.argv) != 3:
-		raise AssertionError("the arguments are bad")
-	
-	try:
-		string = str(sys.argv[1])
-	except ValueError:
-		raise AssertionError("the arguments are bad")
 
-	try:
-		n = int(sys.argv[2])
-	except ValueError:
-		raise AssertionError("the arguments are bad")
-	
-	word = string.split()
-	res = [i for i in ft_filter(lambda word: len(word) > n, word)]
-	print(res)
+def main():
+    """
+    Module main <filterstring>
+
+    the program output a list of words from S,
+    that have a length greater than N.
+    check all the error , raises exception , call the ft_filter function
+
+    Args:
+        none, the two string are taken by sys.argv
+
+    Returns:
+        none
+
+    Raises:
+        AssertionError: if the number of args is wrong
+        or if there are not valid
+    """
+
+    if len(sys.argv) != 3:
+        raise AssertionError("the arguments are bad")
+
+    try:
+        string = str(sys.argv[1])
+    except ValueError:
+        raise AssertionError("the arguments are bad")
+
+    try:
+        n = int(sys.argv[2])
+    except ValueError:
+        raise AssertionError("the arguments are bad")
+
+    word = string.split()
+    # comprehension list
+    res = [i for i in ft_filter(lambda word: len(word) > n, word)]
+    print(res)
+
 
 if __name__ == "__main__":
-	try:
-		main()
-	except AssertionError as e:
-		print(f"AssertionError: {e}")
+    try:
+        main()
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
--- py-00/ex06/ft_filter.py
+++ py-00/ex06/ft_filter.py
@@ -1,4 +1,19 @@
 def ft_filter(function, word):
+    """
+    Apply a filtering function to a list of words.
+
+    This function behaves like Python's built-in filter: it returns
+    an iterator containing only the elements for which `function`
+    returns True. If function is None, all truthy elements are returned.
+
+    Args:
+        function (callable or None): A function applied to each element.
+        word (iterable): A sequence of strings to filter.
+
+    Returns:
+        iterator: An iterator of the filtered elements.
+    """
+    # generator expression
     if function is None:
         return (index for index in word if index)
     return (index for index in word if function(index))
